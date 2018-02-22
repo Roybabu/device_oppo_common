e89fc7a937be0ac5e145b652b26ff157d7c619be
Anton Hansson <hansson@google.com>
2018-02-22 17:13:00 +0100

    configpanel: Set LOCAL_PRIVATE_PLATFORM_APIS
    This change sets LOCAL_SDK_VERSION for all packages where
    this is possible without breaking the build, and
    LOCAL_PRIVATE_PLATFORM_APIS := true otherwise.
    
    Setting one of these two will be made required soon, and this
    is a change in preparation for that. Not setting LOCAL_SDK_VERSION
    makes the app implicitly depend on the bootclasspath, which is
    often not required. This change effectively makes depending on
    private apis opt-in rather than opt-out.
    
    Test: make relevant package
    Bug: 73535841
    Change-Id: I5196e8c7277ff86280782e7b479a499add2e8f9f
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- configpanel/Android.mk
+++ configpanel/Android.mk
@@ -22,6 +22,21 @@ LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PACKAGE_NAME := ConfigPanel
 LOCAL_MODULE_TAGS := optional
+LOCAL_PRIVATE_PLATFORM_APIS := true
+
+LOCAL_USE_AAPT2 := true
+
+LOCAL_STATIC_ANDROID_LIBRARIES := \
+    android-support-v4 \
+    android-support-v7-appcompat \
+    android-support-v7-preference \
+    android-support-v7-recyclerview \
+    android-support-v13 \
+    android-support-v14-preference
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    org.lineageos.platform.internal
+>>>>>>> faeefcd... configpanel: Set LOCAL_PRIVATE_PLATFORM_APIS
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
